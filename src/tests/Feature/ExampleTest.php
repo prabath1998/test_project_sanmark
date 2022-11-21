94b778a30911e803a08b65a3375171945814f137
prabath1998 <prabhathudayanga1998@gmail.com>
2022-11-21 10:04:45 +0530

    user can store new record - tested

--- src/tests/Feature/ExampleTest.php
+++ src/tests/Feature/ExampleTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -12,10 +13,27 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function test_the_application_returns_a_successful_response()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
-    }
-}
+    use WithoutMiddleware;
+    // public function test_the_application_returns_a_successful_response()
+    // {
+    //     $response = $this->get('/');
+
+    //     $response->assertStatus(200);
+    // }
+
+    public function test_user_can_store_new_record()
+    {
+       
+       $response = $this->post('/store',[
+          'name' => 'Asanka',
+          'radius' => '666',
+          'begin' =>'09:00:00',
+          'end' => '08:01:00',
+          'laps' => 2,
+       ]);
+ 
+       $response->assertRedirect('/');      
+   
+
+    }}
